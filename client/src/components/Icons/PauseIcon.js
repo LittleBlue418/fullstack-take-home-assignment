cc657e19f911373039dbdb4d45d1bf6ab81c5093
Holly Thomas <holly@fishbrain.com>
2024-04-19 19:06:39 +0200

    Create reusable Icons

---
+++ client/src/components/Icons/PauseIcon.js
@@ -0,0 +1,17 @@
+export const PauseIcon = ({ size = "24", ariaLabel = "Add" }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-labelledby={ariaLabel}
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M10 5H7V19H10V5ZM17 5H14V19H17V5Z"
+      fill="#333333"
+    />
+  </svg>
+);
---
+++ client/src/components/Icons/PlayIcon.js
@@ -0,0 +1,12 @@
+export const PlayIcon = ({ size = "24", ariaLabel = "Add" }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-labelledby={ariaLabel}
+  >
+    <path d="M20 12L8 5V19L20 12Z" fill="white" />
+  </svg>
+);
