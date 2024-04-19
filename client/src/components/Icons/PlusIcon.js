48084c69c7671831ac25847c07365fe09a0d64d8
Holly Thomas <holly@fishbrain.com>
2024-04-19 22:26:30 +0200

    New plus icon

--- client/src/components/Icons/PlusIcon.js
+++ client/src/components/Icons/PlusIcon.js
@@ -1,7 +1,4 @@
-export const PlusIcon = ({
-  size = '24',
-  ariaLabel = "Add",
-}) => (
+export const PlusIcon = ({ size = "24", ariaLabel = "Add" }) => (
   <svg
     width={size}
     height={size}
@@ -10,6 +7,19 @@ export const PlusIcon = ({
     xmlns="http://www.w3.org/2000/svg"
     aria-labelledby={ariaLabel}
   >
-    <path d="M20 12L8 5V19L20 12Z" fill="white" />
+    <path
+      d="M18.6569 11.6569H5.34315"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="stroke-current"
+    />
+    <path
+      d="M12 5V18.3137"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="stroke-current"
+    />
   </svg>
 );
